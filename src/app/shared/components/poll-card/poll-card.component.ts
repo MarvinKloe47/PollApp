48e0cac66bc2c3f0eadf79463d80a212831bd8fe
Marvin Kloe <marvin.kloesters@omexom.com>
2026-04-24 10:08:20 +0200

    feat: add poll card component

---
+++ src/app/shared/components/poll-card/poll-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Poll } from '../../../core/models/poll.model';
+
+@Component({
+  selector: 'app-poll-card',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './poll-card.component.html',
+  styleUrl: './poll-card.component.scss'
+})
+export class PollCardComponent {
+  @Input() poll!: Poll;
+
+  get totalVotes(): number {
+    return this.poll.options.reduce(
+      (sum, option) => sum + option.voteCount,
+      0
+    );
+  }
+
+  get isActive(): boolean {
+    return new Date(this.poll.deadline) > new Date();
+  }
+}
